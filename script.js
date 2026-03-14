d7ff36b70fca12af03a3c06f5c0e3d499df00fbb
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 13:21:45 +0300

    Дан массив с числами. Подсчитайте количество отрицательных чисел в этом массиве.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 let arr = [1, 2, 3, 4, 5, 6];
-let half = arr.length / 2;
-let sum = 0;
-for (let i = 0; i < half; i++) {
-  sum += arr[i];
-}
+let negativeNumbers = arr.filter((item) => item < 0);
+console.log(negativeNumbers.length);
