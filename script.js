a75b4fd0d026f04e443c53ef7168bc66f7cbe2a6
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 08:38:14 +0300

    Дан массив с дробями:
    [1.456, 2.125, 3.32, 4.1, 5.34]
    Округлите эти дроби до одного знака в дробной части.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let arr = [];
-
-for (let i = 1; i <= 100; i++) {
-  if (i % 2 === 0) {
-    arr.push(i);
-  }
-}
-
-console.log(arr);
+let numbers = [1.456, 2.125, 3.32, 4.1, 5.34];
+let roundet = numbers.map((num) => Math.round(num * 10) / 10);
+console.log(roundet);
