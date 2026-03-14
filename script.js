b35d0be6423207c8c53151205134b396761e6761
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 09:45:45 +0300

    Заполните массив случайными числами из промежутка от 1 до 100.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let numbers = [4, 5, 6, 7, 8];
-let increased = numbers.map((num) => num * 1.1);
-console.log(increased);
+let arr = [];
+for (i = 0; i < 10; i++) {
+  let randomNum = Math.floor(Math.random() * 100) + 1;
+  arr.push(randomNum);
+}
+console.log(arr);
