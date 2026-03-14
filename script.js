6969c3587a1254297509a4866d07ae26fdbb2ce0
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 19:23:10 +0300

    Дан некоторый массив, например, вот такой:
    [1, 2, 3, 4, 5, 6]
    Поделите сумму элементов, стоящих на четных позициях, на сумму элементов, стоящих на нечетных позициях.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let str = "abcdefg";
-let result = "";
-for (let i = 0; i < str.length; i++) {
-  if ((i + 1) % 3 !== 0) {
-    result += str[i];
+let str = [1, 2, 3, 4, 5, 6];
+evenNum = 0;
+oddNum = 0;
+for (i = 0; i < str.length; i++) {
+  if (i % 2 === 0) {
+    evenNum += str[i];
+  } else {
+    oddNum += str[i];
   }
 }
+let result = evenNum / oddNum;
 console.log(result);
