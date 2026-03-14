6ecb89c94050342bab1d10824a6322029929660a
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 13:03:38 +0300

    Дан некоторый массив, например, вот такой:
    [1, 2, 3, 4, 5, 6]
    Найдите сумму первой половины элементов этого массива.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let arr = [1, 2, 3, 2, 4, 2, 5];
-let valuToRemove = 2;
-
-let filtered = arr.filter((item) => item !== valueToRemove);
-console.log(filtered);
-console.log(arr);
+let arr = [1, 2, 3, 4, 5, 6];
+let half = arr.length / 2;
+let sum = 0;
+for (let i = 0; i < half; i++) {
+  sum += arr[i];
+}
