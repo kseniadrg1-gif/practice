00ce1366cb622a914f3ca7e750773bafad98ad71
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 09:18:22 +0300

    Дан массив со строками. Оставьте в этом массиве только те строки, которые заканчиваются на .html.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let strings = [
   "http://site.com",
-  "https://google.com",
+  "https://google.html",
   "http://blog.ru",
   "ftp://files.net",
   "http://shop.org",
   "просто текст",
   "http://news.com/page",
 ];
-let httpSites = strings.filter((str) => str.startsWith("http://"));
+let httpSites = strings.filter((str) => str.endsWith(".html"));
 console.log(httpSites);
