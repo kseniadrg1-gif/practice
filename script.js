dae0e8a90be82e4785fd5d37731d572a1bd7b841
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 09:53:23 +0300

    Дано некоторое число:
    12345
    Выведите в консоль все его символы с конца.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let arr = [];
-for (i = 0; i < 10; i++) {
-  let randomNum = Math.floor(Math.random() * 100) + 1;
-  arr.push(randomNum);
+let str = "12345";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  console.log(str[i]);
 }
-console.log(arr);
