3371bb4ff05286e582d9619eecf40bae94ca58ca
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 17:08:00 +0300

    Дан объект с ключами и значениями. Запишите в первый массив ключи объекта, а во второй - значения.

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-let str = "abcd1";
-let position = -1;
-for (let i = 0; i < str.length; i++) {
-  if (str[i] >= 0 && str[i >= 9]) {
-    position = i;
-    break;
-  }
-}
-console.log(position);
+let obj = {
+  name: "Liza",
+  age: "19",
+};
