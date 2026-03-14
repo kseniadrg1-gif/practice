21e9f29dbca7268d8ded83e59bdc3d96dc5ca1b4
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 13:37:11 +0300

    Дан некоторый массив, например, вот такой:
    [1, 2, 3, 4, 5, 6]
    Поделите сумму первой половины элементов этого массива на сумму второй половины элементов.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let str = "Hello";
-console.log(str.at(-1));
+let arr = [1, 2, 3, 4, 5, 6];
+let half = arr.length / 2;
+let sumFirst = 0;
+for (let i = 0; i < half; i++) {
+  sumFirst += arr[i];
+}
+let sumSecond = 0;
+for (let i = half; i < arr.length; i++) {
+  sumSecond += arr[i];
+}
+
+let result = sumFirst / sumSecond;
+console.log(result);
