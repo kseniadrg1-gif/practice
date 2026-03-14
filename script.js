58af42f74fb1d822c26bf07f97714cb46c62fdad
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 17:15:21 +0300

    Дано число. Выведите в консоль количество четных цифр в этом числе.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let obj = {
-  name: "Liza",
-  age: "19",
-};
+let numbers = 123456;
+let str = String(num);
+let count = 0;
+for (let i = 0; i < str.length; i++) {
+  let digit = Number(str[i]);
+  if (digit % 2 === 0) {
+    count++;
+  }
+}
+console.log(count);
