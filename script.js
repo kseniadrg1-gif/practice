3c47424a00593773f278be29192f70a6f5709c8b
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 13:25:27 +0300

    Дан массив с числами. Оставьте в нем только положительные числа.

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let arr = [1, 2, 3, 4, 5, 6];
-let negativeNumbers = arr.filter((item) => item < 0);
-console.log(negativeNumbers.length);
+let positively = arr.filter((item) => item > 0);
+console.log(positively);
