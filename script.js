1a6552981dc2636bdbc535f05358fad946336982
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-14 14:51:58 +0300

    Дана дата в следующем формате:
    '2025-12-31'
    Преобразуйте эту дату в следующий объект:
    
    {
        year: '2025',
        month: '12',
        day: '31',
    }

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let numbers = "12,34,56";
-let sum = numbers
-  .split(",")
-  .map(Number)
-  .reduce((a, b) => a + b, 0);
-console.log(sum);
+let dateStr = "2025-12-31";
+let [year, month, day] = dateStr.split("-");
+let dateObj = {
+  year: year,
+  month: month,
+  day: day,
+};
+console.log(dateObj);
