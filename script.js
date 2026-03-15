6ebeb4acec22460d1eacd4f5a1bf91d9c42efcbc
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-15 08:58:15 +0300

    Дан массив с некоторыми числами, например, вот такой:
    [123, 456, 789]
    Напишите код, который перевернет числа в этом массиве по следующему принципу:
    
    [321, 654, 987]

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let str = "dfgr347cvvf9df6df";
-let position = [];
-for (let i = 0; i < str.length; i++) {
-  let char = str[i];
-  if (char >= "0" && char <= "9") {
-    position.push(i);
-  }
+let arr = [123, 345, 789];
+let reversedArr = [];
+for (i = 0; i < arr.length; i++) {
+  let num = arr[i];
+  let reversedNum = Number(String(num).split("").reverse().join(""));
+  reversedArr.push(reversedNum);
 }
-console.log(position);
+console.log(reversedArr);
