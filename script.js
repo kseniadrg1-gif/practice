fcd1f671857961c4980e845c08bf9c418491b80c
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-15 10:09:32 +0300

    Дана некоторая строка со словами:
    'aaa bbb ccc eee fff'
    Сделайте заглавным первый символ каждого второго слова в этой строке. В нашем случае должно получится следующее:
    
    'aaa Bbb ccc Eee fff'

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let arr = [1, 2, 3, 4, 5, 6];
+let str = "aaa bbb ccc eee fff";
+let words = str.split("");
 let result = [];
-for (let i = 0; i < arr.length; i += 2) {
-  let first = arr[i];
-  let second = arr[i + 1];
-  let combined = String(first) + String(second);
-  result.push(Number(combined));
+for (let i = 0; i < str.length; i++) {
+  if (i % 2 === 1) {
+    let word = words[i];
+    let capitalized = word[0].toApperCase() + word.slise(1);
+    result.push(words[i]);
+  }
 }
-console.log(result);
+let finalStr = result.join(" ");
+console.log(finalStr);
