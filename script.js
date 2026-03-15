13cde33f4acd1be04b0479a742881e74e940f00f
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-15 09:57:35 +0300

    Дан некоторый массив с числами, например, вот такой:
    [1, 2, 3, 4, 5, 6]
    Слейте пары элементов вместе:
    
    [12, 34, 56]

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let str = "AbCdE";
-let result = "";
-for (i = 0; i < str.length; i++) {
-  let char = str[i];
-  if (char === char.toUpperCase()) {
-    result += char.toLowerCase();
-  } else {
-    result += char.toUpperCase();
-  }
+let arr = [1, 2, 3, 4, 5, 6];
+let result = [];
+for (let i = 0; i < arr.length; i += 2) {
+  let first = arr[i];
+  let second = arr[i + 1];
+  let combined = String(first) + String(second);
+  result.push(Number(combined));
 }
 console.log(result);
